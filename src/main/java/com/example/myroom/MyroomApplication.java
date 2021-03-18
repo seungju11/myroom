050f30bc69a4ea505686b7b4f2accdc5c247c497
dja607 <dja607@naver.com>
2021-03-18 19:26:34 +0900

    init

---
+++ src/main/java/com/example/myroom/MyroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.myroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myroom/MyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
